07f0909ad8678c264df211ba0c29039cee60572f
Бойко Боев <boykopetevboev@gmail.com>
2024-09-30 00:01:27 +0300

    templates basics practice

--- templates_basics/templates_basics/posts_app/apps.py
+++ templates_basics/templates_basics/posts_app/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class PostsAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'posts_app'
+    name = 'templates_basics.posts_app'
